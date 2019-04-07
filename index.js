c24f4fe0a28c23de48141e2a4ce49cb5545d529e
retrohacker <code@blankenship.io>
2019-04-07 14:48:05 -0700

    conditionally wrap and support yarn

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const {spawn} = require('child_process')
-const args = process.argv.slice(2)
+let args = process.argv.slice(2)
+let command = 'npm'
+if(args[0] === '--yarn') {
+  args = args.slice(1)
+  command = 'yarn'
+}
 const env = JSON.parse(JSON.stringify(process.env))
-env['npm_config_registry'] = 'http://mdm.blankenship.io'
+const dontWrap = [
+  'access',
+  'adduser',
+  'deprecate',
+  'dist-tag',
+  'login',
+  'logout',
+  'publish',
+  'star',
+  'stars',
+  'team',
+  'token',
+  'unpublish',
+  'unstar',
+  'whoami'
+]
 
-spawn('npm', args, {
-    stdio: [
-      process.stdin,
-      process.stdout,
-      process.stderr
-    ], env
+if(dontWrap.indexOf(String(args[0]).toLowerCase()) === -1) {
+  env['npm_config_registry'] = 'http://mdm.blankenship.io'
+}
+
+spawn(command, args, {
+  stdio: [
+    process.stdin,
+    process.stdout,
+    process.stderr
+  ], env
 })
